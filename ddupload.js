2be31cb3ab398019f3ffde96aefd16f3836a0961
iwata5342 <126980328+iwata5342@users.noreply.github.com>
2023-03-07 21:58:22 +0900

    Add files via upload

--- ddupload.js
+++ ddupload.js
@@ -232,3 +232,51 @@ const updateFileList = () => {
     fl.appendChild(li)
   }
 }
+
+// ドラッグ&ドロップエリアの取得
+var fileArea = document.getElementById('dropArea');
+
+// input[type=file]の取得
+var fileInput = document.getElementById('uploadFile');
+
+// ドラッグオーバー時の処理
+fileArea.addEventListener('dragover', function(e){
+    e.preventDefault();
+    fileArea.classList.add('dragover');
+});
+
+// ドラッグアウト時の処理
+fileArea.addEventListener('dragleave', function(e){
+    e.preventDefault();
+    fileArea.classList.remove('dragover');
+});
+
+// ドロップ時の処理
+fileArea.addEventListener('drop', function(e){
+    e.preventDefault();
+    fileArea.classList.remove('dragover');
+
+    // ドロップしたファイルの取得
+    var files = e.dataTransfer.files;
+
+    // 取得したファイルをinput[type=file]へ
+    fileInput.files = files;
+    
+    if(typeof files[0] !== 'undefined') {
+        //ファイルが正常に受け取れた際の処理
+    } else {
+        //ファイルが受け取れなかった際の処理
+    }
+});
+
+// input[type=file]に変更があれば実行
+// もちろんドロップ以外でも発火します
+fileInput.addEventListener('change', function(e){
+    var file = e.target.files[0];
+    
+    if(typeof e.target.files[0] !== 'undefined') {
+        // ファイルが正常に受け取れた際の処理
+    } else {
+        // ファイルが受け取れなかった際の処理
+    }
+}, false);
